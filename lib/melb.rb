91f213b49267b2cf030c97bcc48585ffb37f3204
twistedpixelpete <pete@twistedpixel.com.au>
2021-04-06 17:46:07 +1000

    working through selections

--- lib/melb.rb
+++ lib/melb.rb
@@ -4,11 +4,16 @@ require 'byebug'
 
 class Melb
 
-    attr_reader :activities
+    attr_reader :today_activities, :favs, :today_titles, :list
 
     def initialize
-        @activities = []
+        @today_activities = []
+        @today_titles =[]
         scrape_today
+        @favs = []
+        @combine_today_description
+        @list = []
+        
         
     end
     
@@ -20,10 +25,10 @@ class Melb
         items = parsed_page.css('div.card-content')
         items.each do |activity|
             activity = {
-                title: activity.css('h3.card-title').text,
+                title: activity.css('h3.card-title').text.strip,
                 description: activity.css('p').text
             }  
-            @activities << activity
+            @today_activities << activity
             
             
         end    
@@ -32,13 +37,46 @@ class Melb
 
     def lists_today_activities
         i =0
-         while i < activities.length-1
-            title = activities[i][:title]
+         while i < today_activities.length-1
+            title = today_activities[i][:title]
             puts "#{title.strip}"
-
+            
             i += 1
          end
         end
+
+        def combine_today_titles
+            i =0
+             while i < today_activities.length-1
+                title = today_activities[i][:title]
+                
+                @today_titles << title
+                i += 1
+             end
+        end
+
+        def combine_today_description
+            i =0
+             while i < today_activities.length-1
+                description = today_activities[i][:description]
+                
+                @combine_today_description << description
+                i += 1
+            end
+            
+        end
+
+        def activity_display
+            i=0
+           while i < today_activities.length-1
+               
+              bob = {name: today_activities[i][:title], value: i+1.to_i}
+
+              @list << bob
+
+                i += 1     
+            end
+        end 
     
 end
 
--- main.rb
+++ main.rb
@@ -7,14 +7,55 @@ require_relative './lib/interface'
 whats_on = Melb.new
 interface = Interface.new 
 prompt = TTY::Prompt.new
-app = true
 
 
 
-def melb_list_today(interface, whats_on)
+
+def melb_list_today(interface, whats_on, prompt)
     interface.title_block("Lets find something fun to do today!")
-    whats_on.lists_today_activities
+    whats_on.activity_display
+    
+    title = whats_on.list
+#    puts title
+    chosen_activity = prompt.select("Select an activity to find out more", title.push({name: "Exit", value: 11}))
+
+    case chosen_activity
+    when 1
+        puts whats_on.today_activities[0][:description]
+        
+    when 2
+        puts whats_on.today_activities[1][:description]
+
+    when 3 
+        puts whats_on.today_activities[2][:description]
+    when 4 
+        puts whats_on.today_activities[3][:description]
+    when 5
+        puts whats_on.today_activities[4][:description]
+    when 6 
+        puts whats_on.today_activities[5][:description]
+    when 7 
+        puts whats_on.today_activities[6][:description]
+    when 8 
+        puts whats_on.today_activities[7][:description]
+    when 9 
+        puts whats_on.today_activities[8][:description]
+    when 10 
+        puts whats_on.today_activities[9][:description]
+    when 11 
+        interface.welcome
+        
+    else 
+        interface.welcome
+        
+    end
+  
 end
+
+  # p titles
+    # whats_on.lists_today_activities
+    
+    # puts whats_on.today_titles
 
 def melb_list_weekend(interface)
     interface.title_block("What should we do on the weekend???")
@@ -39,13 +80,19 @@ end
 
 
     interface.welcome
-    
     sleep(0.6)
-    prompt.select("What would you like to do?") do |menu|
-       menu.choice "Check out what's on today", -> { melb_list_today(interface, whats_on)} 
-       menu.choice "Check out things to do on the weekend", -> { melb_list_weekend(interface) }
-       menu.choice "Look at my favorites", -> { favorites_list(interface) }
-       menu.choice "Exit", -> { leave_app(interface) }
+
+    
+
+        prompt.select("What would you like to do?") do |menu|
+            menu.choice "Check out what's on today", -> { melb_list_today(interface, whats_on,prompt) } 
+            menu.choice "Check out things to do on the weekend", -> { melb_list_weekend(interface) }
+            menu.choice "Look at my favorites", -> { favorites_list(interface) }
+            menu.choice "Exit", -> { leave_app(interface) }
+        
+    
+    
+    
      end 
 
 
